bd6910794e455ae27b8e7a4d83cfb5174cba6954
manelaskry <manlaskry8@gmail.com>
2025-03-13 17:14:50 +0100

    init-taiwind-dependencies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Auth from "./pages/auth";
+//import AdminnDashboard from "./pages/dash";
+import AdminDashboard from "./pages/dashboard";
+import ProjectAccessManagement from "./pages/manageProfil";
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/edituser" element={<ProjectAccessManagement />} />
+      </Routes>
+    </Router>
+  );
+}
